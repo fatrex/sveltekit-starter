20f455c03fdc43d9d618b9eb5932ed7854b2f967
Daniele Lenares <hi@dnlnrs.dev>
2022-04-03 07:33:15 +0200

    Init repo

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+  import '../app.css'
+</script>
+
+<div class="container mx-auto text-center text-lg">
+  <h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
+  <p class="pt-10">
+    This is a SvelteKit startert with:
+  </p>
+  <ul>
+    <li>Sveltekit</li>
+    <li>TypeScript</li>
+    <li>Playwright</li>
+    <li>Tailwind</li>
+  </ul>
+</div>
+
